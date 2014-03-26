e8de4b16d39d810917f8c70c6c739c5538c7015d
Oliver Hoff <oliver@hofff.com>
2014-03-26 22:31:20 +0100

    add autoload files

---
+++ contao-module/config/autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package Cca-language-relations
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'cca_lr_groupRoots' => 'system/modules/cca-language-relations/templates',
+	'cca_lr_pageInfo'   => 'system/modules/cca-language-relations/templates',
+));
