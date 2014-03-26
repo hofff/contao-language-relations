e03d2847e1373c4299313014996fb9464d814c5d
Andreas Isaak <andy.jared@googlemail.com>
2014-03-27 00:32:40 +0100

    Add missing translations, reindent code

--- contao-module/languages/de/tl_cca_lr_group.php
+++ contao-module/languages/de/tl_cca_lr_group.php
@@ -1,21 +1,21 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['new']
-	= array('Neue Übersetzungsgruppe', 'Eine neue Übersetzungsgruppe anlegen');
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['edit']
-	= array('Übersetzungsgruppe bearbeiten', 'Die Übersetzungsgruppe ID %s bearbeiten');
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['delete']
-	= array('Übersetzungsgruppe löschen', 'Die Übersetzungsgruppe ID %s löschen');
+/**
+* Legends
+*/
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['general_legend']    = 'Allgemein';
 
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['general_legend']
-	= 'Allgemein';
+/**
+* List operation
+*/
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['new']               = array('Neue Übersetzungsgruppe', 'Eine neue Übersetzungsgruppe anlegen');
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['edit']              = array('Übersetzungsgruppe bearbeiten', 'Die Übersetzungsgruppe ID %s bearbeiten');
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['delete']            = array('Übersetzungsgruppe löschen', 'Die Übersetzungsgruppe ID %s löschen');
 
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['title']
-	= array('Titel', 'Der Name dieser Übersetzungsgruppe');
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['roots']
-	= array('Startpunkte', 'Die Startpunktseiten die zu dieser Übersetzungsgruppe gehören.');
-
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['notGrouped']
-	= 'Keiner Gruppe zugewiesen';
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['noPagesToEdit']
-	= 'Keine Seiten zu bearbeiten';
+/**
+* Fields
+*/
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['title']             = array('Titel', 'Der Name dieser Übersetzungsgruppe');
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['roots']             = array('Startpunkte', 'Die Startpunktseiten die zu dieser Übersetzungsgruppe gehören.');
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['notGrouped']        = 'Keiner Gruppe zugewiesen';
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['noPagesToEdit']     = 'Keine Seiten zu bearbeiten';
--- contao-module/languages/de/tl_page.php
+++ contao-module/languages/de/tl_page.php
@@ -1,8 +1,12 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_page']['cca_lr_legend']
-	= 'Übersetzungen';
-$GLOBALS['TL_LANG']['tl_page']['cca_lr_pageInfo']
-	= array('Seiteninformationen', '');
-$GLOBALS['TL_LANG']['tl_page']['cca_lr_relations']
-	= array('Übersetzungen', 'Legt fest, welche Seiten anderer Startpunkte Übersetzungen dieser Seite sind.');
+/**
+* Legends
+*/
+$GLOBALS['TL_LANG']['tl_page']['cca_lr_legend']             = 'Übersetzungen';
+
+/**
+* Fields
+*/
+$GLOBALS['TL_LANG']['tl_page']['cca_lr_pageInfo']           = array('Seiteninformationen', '');
+$GLOBALS['TL_LANG']['tl_page']['cca_lr_relations']          = array('Übersetzungen', 'Legt fest, welche Seiten anderer Startpunkte Übersetzungen dieser Seite sind.');
