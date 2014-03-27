0642fcdf915b23014c955bd5f93730ffd7548e38
Oliver Hoff <oliver@hofff.com>
2014-03-27 08:52:27 +0100

    validate selection and output error messages

--- contao-module/languages/de/tl_page.php
+++ contao-module/languages/de/tl_page.php
@@ -1,12 +1,19 @@
 <?php
 
 /**
-* Legends
-*/
-$GLOBALS['TL_LANG']['tl_page']['cca_lr_legend']             = 'Übersetzungen';
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_page']['cca_lr_legend']                      = 'Übersetzungen';
 
 /**
-* Fields
-*/
-$GLOBALS['TL_LANG']['tl_page']['cca_lr_pageInfo']           = array('Seiteninformationen', '');
-$GLOBALS['TL_LANG']['tl_page']['cca_lr_relations']          = array('Übersetzungen', 'Legt fest, welche Seiten anderer Startpunkte Übersetzungen dieser Seite sind.');
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_page']['cca_lr_pageInfo']                    = array('Seiteninformationen', '');
+$GLOBALS['TL_LANG']['tl_page']['cca_lr_relations']                   = array('Übersetzungen', 'Legt fest, welche Seiten anderer Startpunkte Übersetzungen dieser Seite sind.');
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_page']['cca_lr_errMultipleRelationsPerRoot'] = 'Es darf je Startpunkt nur eine Referenz ausgewählt werden.';
+$GLOBALS['TL_LANG']['tl_page']['cca_lr_errUngroupedRelations']       = 'Die Startpunkte aller referenzierten Seiten müssen der gleichen Übersetzungsgruppe angehören.';
+$GLOBALS['TL_LANG']['tl_page']['cca_lr_errOwnRootRelations']         = 'Es darf sich keine referenzierte Seite im gleichen Startpunkt wie diese Seite befinden.';
--- src/ContaoCommunityAlliance/Contao/LanguageRelations/PageDCA.php
+++ src/ContaoCommunityAlliance/Contao/LanguageRelations/PageDCA.php
@@ -20,7 +20,7 @@ class PageDCA {
 			$sql = 'DELETE FROM tl_cca_lr_relation WHERE pageFrom = ?';
 			\Database::getInstance()->prepare($sql)->executeUncached($dc->id);
 
-			$relations = deserialize($this->relations[$dc->id], true);
+			$relations = $this->relations[$dc->id];
 			if($relations) {
 				$wildcards = rtrim(str_repeat('(?,?),', count($relations)), ',');
 				$sql = 'INSERT INTO tl_cca_lr_relation (pageFrom, pageTo) VALUES ' . $wildcards;
@@ -45,6 +45,50 @@ class PageDCA {
 	}
 
 	public function saveRelations($value, $dc) {
+		$value = deserialize($value, true);
+
+		if($value) {
+			$wildcards = rtrim(str_repeat('?,', count($value)), ',');
+
+			$sql = <<<SQL
+SELECT		cca_rr_root
+FROM		tl_page
+WHERE		id IN ($wildcards)
+GROUP BY	cca_rr_root
+HAVING		COUNT(id) > 1
+LIMIT		1
+SQL;
+			$params = $value;
+			$result = \Database::getInstance()->prepare($sql)->executeUncached($params);
+			if($result->numRows) {
+				throw new \Exception($GLOBALS['TL_LANG']['tl_page']['cca_lr_errMultipleRelationsPerRoot']);
+			}
+
+			$sql = <<<SQL
+SELECT		SUM(rootPage.cca_lr_group != curRootPage.cca_lr_group) AS ungroupedRelations,
+			SUM(rootPage.id = curRootPage.id) AS ownRootRelations
+
+FROM		tl_page		AS page
+JOIN		tl_page		AS rootPage			ON rootPage.id = page.cca_rr_root
+JOIN		(
+	SELECT	curRootPage1.cca_lr_group, curRootPage1.id
+	FROM	tl_page		AS curPage1
+	JOIN	tl_page		AS curRootPage1		ON curRootPage1.id = curPage1.cca_rr_root
+	WHERE	curPage1.id = ?
+)						AS curRootPage
+
+WHERE		page.id IN ($wildcards)
+SQL;
+			array_unshift($params, $dc->id);
+			$result = \Database::getInstance()->prepare($sql)->executeUncached($params);
+			if($result->ungroupedRelations) {
+				throw new \Exception($GLOBALS['TL_LANG']['tl_page']['cca_lr_errUngroupedRelations']);
+			}
+			if($result->ownRootRelations) {
+				throw new \Exception($GLOBALS['TL_LANG']['tl_page']['cca_lr_errOwnRootRelations']);
+			}
+		}
+
 		$this->relations[$dc->id] = $value;
 		return null;
 	}
