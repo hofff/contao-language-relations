ca7881f872b0b7363a0ac4e13e38d287341d2f56
Oliver Hoff <oliver@hofff.com>
2014-03-27 02:18:27 +0100

    move translation tokens of group listing into group dca translation file and use translation tokens for all text in the listing

--- contao-module/languages/de/default.php
+++ contao-module/languages/de/default.php
@@ -1,8 +1,1 @@
 <?php
-
-/**
-* Fields
-*/
-$GLOBALS['TL_LANG']['MSC']['cca_lr_group']                  = 'Übersetzungsreferenzen bearbeiten';
-$GLOBALS['TL_LANG']['MSC']['cca_lr_group_incomplete']       = 'Unvollständige Übersetzungsreferenzen bearbeiten';
-$GLOBALS['TL_LANG']['MSC']['cca_lr_group_ambiguous']        = 'Fehlerhafte Übersetzungsreferenzen bearbeiten';
--- contao-module/languages/de/tl_cca_lr_group.php
+++ contao-module/languages/de/tl_cca_lr_group.php
@@ -1,21 +1,29 @@
 <?php
 
 /**
-* Legends
-*/
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['general_legend']    = 'Allgemein';
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['general_legend']          = 'Allgemein';
 
 /**
-* List operation
-*/
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['new']               = array('Neue Übersetzungsgruppe', 'Eine neue Übersetzungsgruppe anlegen');
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['edit']              = array('Übersetzungsgruppe bearbeiten', 'Die Übersetzungsgruppe ID %s bearbeiten');
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['delete']            = array('Übersetzungsgruppe löschen', 'Die Übersetzungsgruppe ID %s löschen');
+ * List operation
+ */
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['new']                     = array('Neue Übersetzungsgruppe', 'Eine neue Übersetzungsgruppe anlegen');
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['edit']                    = array('Übersetzungsgruppe bearbeiten', 'Die Übersetzungsgruppe ID %s bearbeiten');
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['delete']                  = array('Übersetzungsgruppe löschen', 'Die Übersetzungsgruppe ID %s löschen');
 
 /**
-* Fields
-*/
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['title']             = array('Titel', 'Der Name dieser Übersetzungsgruppe');
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['roots']             = array('Startpunkte', 'Die Startpunktseiten die zu dieser Übersetzungsgruppe gehören.');
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['notGrouped']        = 'Keiner Gruppe zugewiesen.';
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['noPagesToEdit']     = 'Alle Seiten wurden bereits zugeordnet.';
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['title']                   = array('Titel', 'Der Name dieser Übersetzungsgruppe');
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['roots']                   = array('Startpunkte', 'Die Startpunktseiten die zu dieser Übersetzungsgruppe gehören.');
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['notGrouped']              = 'Keiner Gruppe zugewiesen.';
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['noPagesToEdit']           = 'Alle Seiten wurden bereits zugeordnet.';
+
+/**
+ * Misc
+ */
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['noRootsAssigned']          = 'Keine Startpunkte zugewiesen';
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['editRelations']            = 'Übersetzungsreferenzen bearbeiten';
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['editIncompleteRelations']  = 'Unvollständige Übersetzungsreferenzen bearbeiten';
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['editAmbiguousRelations']   = 'Fehlerhafte Übersetzungsreferenzen bearbeiten';
