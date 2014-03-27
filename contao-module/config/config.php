4a635aca8d636aac17f37d70b1a3a08583f83a3d
Oliver Hoff <oliver@hofff.com>
2014-03-27 01:18:46 +0100

    add icon and stylesheet for be module

--- contao-module/config/config.php
+++ contao-module/config/config.php
@@ -2,4 +2,6 @@
 
 $GLOBALS['BE_MOD']['design']['cca_lr_group']['tables'][]             = 'tl_cca_lr_group';
 $GLOBALS['BE_MOD']['design']['cca_lr_group']['tables'][]             = 'tl_page';
+$GLOBALS['BE_MOD']['design']['cca_lr_group']['icon']                 = 'article.gif';
 $GLOBALS['BE_MOD']['design']['cca_lr_group']['editRelations']        = array('ContaoCommunityAlliance\\Contao\\LanguageRelations\\GroupDCA', 'keyEditRelations');
+$GLOBALS['BE_MOD']['design']['cca_lr_group']['stylesheet'][]         = 'system/modules/cca-language-relations/assets/css/style.css';
