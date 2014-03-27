be9f936c68fdfd17783a6373923ef51c3a6f4755
Andreas Isaak <andy.jared@googlemail.com>
2014-03-27 10:56:38 +0100

    Optimize translation

--- contao-module/languages/de/tl_cca_lr_group.php
+++ contao-module/languages/de/tl_cca_lr_group.php
@@ -3,7 +3,7 @@
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['general_legend']          = 'Allgemein';
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['general_legend']          = 'Allgemeine Einstellungen';
 
 /**
  * List operation
@@ -15,8 +15,8 @@ $GLOBALS['TL_LANG']['tl_cca_lr_group']['delete']                  = array('Über
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['title']                   = array('Titel', 'Der Name dieser Übersetzungsgruppe');
-$GLOBALS['TL_LANG']['tl_cca_lr_group']['roots']                   = array('Startpunkte', 'Die Startpunktseiten die zu dieser Übersetzungsgruppe gehören.');
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['title']                   = array('Titel', 'Bitte geben Sie hier den Titel der Übersetzungsgruppe ein.');
+$GLOBALS['TL_LANG']['tl_cca_lr_group']['roots']                   = array('Startpunkte', 'Bitte wählen Sie hier die Startpunkte aus, die in der Übersetzungsgruppe verwendet werden sollen.');
 $GLOBALS['TL_LANG']['tl_cca_lr_group']['notGrouped']              = 'Keiner Gruppe zugewiesen.';
 
 /**
