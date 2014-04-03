d23f492755bd64100118ab6cebf5b5b14490acfb
Andreas Isaak <andy.jared@googlemail.com>
2014-04-03 17:57:34 +0200

    Delete unused translation and add english translation

---
+++ contao-module/languages/en/modules.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+* Back end modules
+*/
+$GLOBALS['TL_LANG']['MOD']['cca-language-relations']        = 'Language relations for translations';
+$GLOBALS['TL_LANG']['MOD']['cca_lr_group']                  = array('Translation groups', 'Create and configure translation groups');
