8c960a2b222cf70a9ca58c19c028cac487b96a18
Oliver Hoff <oliver@hofff.com>
2016-03-23 18:54:23 +0100

    drop keys before renaming columns

--- runonce/01_renameDatabaseResources.php
+++ runonce/01_renameDatabaseResources.php
@@ -10,6 +10,7 @@ if(Database::getInstance()->tableExists('tl_cca_lr_relation')) {
 
 if(Database::getInstance()->tableExists('tl_page')) {
 	if(Database::getInstance()->fieldExists('cca_lr_group', 'tl_page')) {
+		Database::getInstance()->query('ALTER TABLE tl_page DROP INDEX cca_lr_group');
 		Database::getInstance()->query('ALTER TABLE tl_page CHANGE cca_lr_group hofff_translation_group_id int(10) unsigned NOT NULL default \'0\'');
 	}
 }
