cb7385ac1a8fe4cf9fcdbd0ffcf57e934840c813
Oliver Hoff <oliver@hofff.com>
2016-03-23 19:29:46 +0100

    harden runonce

--- runonce/01_renameDatabaseResources.php
+++ runonce/01_renameDatabaseResources.php
@@ -1,16 +1,20 @@
 <?php
 
-if(Database::getInstance()->tableExists('tl_cca_lr_group')) {
-	Database::getInstance()->query('ALTER TABLE tl_cca_lr_group RENAME tl_hofff_translation_group');
+$db = Database::getInstance();
+
+if($db->tableExists('tl_cca_lr_group')) {
+	$db->query('ALTER TABLE tl_cca_lr_group RENAME tl_hofff_translation_group');
 }
 
-if(Database::getInstance()->tableExists('tl_cca_lr_relation')) {
-	Database::getInstance()->query('ALTER TABLE tl_cca_lr_relation RENAME tl_hofff_page_translation');
+if($db->tableExists('tl_cca_lr_relation')) {
+	$db->query('ALTER TABLE tl_cca_lr_relation RENAME tl_hofff_page_translation');
 }
 
-if(Database::getInstance()->tableExists('tl_page')) {
-	if(Database::getInstance()->fieldExists('cca_lr_group', 'tl_page')) {
-		Database::getInstance()->query('ALTER TABLE tl_page DROP INDEX cca_lr_group');
-		Database::getInstance()->query('ALTER TABLE tl_page CHANGE cca_lr_group hofff_translation_group_id int(10) unsigned NOT NULL default \'0\'');
+if($db->tableExists('tl_page')) {
+	if($db->query('SHOW INDEX FROM tl_page WHERE Key_name = \'cca_lr_group\'')->numRows) {
+		$db->query('ALTER TABLE tl_page DROP INDEX cca_lr_group');
+	}
+	if($db->fieldExists('cca_lr_group', 'tl_page')) {
+		$db->query('ALTER TABLE tl_page CHANGE cca_lr_group hofff_translation_group_id int(10) unsigned NOT NULL default \'0\'');
 	}
 }
